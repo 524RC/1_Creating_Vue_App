50920bab1ff69b051f346ab40580e20350b660c1
524RC <Rchung524@gmail.com>
2024-04-29 14:23:26 -0400

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 const product = 'Socks'
+
+const app = Vue.createApp({
+    data(){
+        return{
+            product: 'chinese babies',
+            description: ' babies from china working hard at temu'
+        }
+    }
+})
+const babies = 'desc'
+
